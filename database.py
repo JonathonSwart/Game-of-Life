a0b4784d6a3417a48f19725d61e1006d6d2207a3
JonathonSwart <jonathonswart17@icloud.com>
2023-01-15 12:18:03 -0500

    GUI Learning

--- database.py
+++ database.py
@@ -11,6 +11,7 @@ conn = sqlite3.connect('address_book.db')
 # Create a cursor
 c = conn.cursor()
 
+# c.execute("ALTER TABLE addresses RENAME COLUMN text TO zipcode")
 # Create table
 # c.execute("""CREATE TABLE addresses(
 #     first_name text,
@@ -20,6 +21,112 @@ c = conn.cursor()
 #     province, text
 #     zipcode integer
 #     )""")
+
+
+# Create an Update function
+def update():
+     # Create a database or connect to one
+    conn = sqlite3.connect('address_book.db')
+    # Create a cursor
+    c = conn.cursor()
+
+    record_id = delete_box.get()
+    c.execute("""UPDATE addresses SET
+    first_name = :first,
+    last_name = :last,
+    address = :address,
+    city = :city,
+    province = :province,
+    zipcode = :zipcode
+
+    WHERE oid = :oid""",
+    {
+    'first': f_name_editor.get(),
+    'last': l_name_editor.get(),
+    'address': address_editor.get(),
+    'city': city_editor.get(),
+    'province': province_editor.get(),
+    'zipcode': zipcode_editor.get(),
+    'oid': record_id
+    })
+    
+
+    # Commit Changes
+    conn.commit()
+    # Close Connection
+    conn.close()
+
+    editor.destroy()
+
+
+# Create Edit function to update a record
+def edit():
+    global editor
+
+    editor = Tk()
+    editor.title("Update A Record")
+    editor.geometry("400x400")
+
+    # Create a database or connect to one
+    conn = sqlite3.connect('address_book.db')
+    # Create a cursor
+    c = conn.cursor()
+
+    record_id = delete_box.get()
+    # Query the database
+    c.execute("SELECT * FROM addresses WHERE oid = " + record_id)
+    records = c.fetchall()
+
+    # Create Global Variables for text box names
+    global f_name_editor
+    global l_name_editor
+    global address_editor
+    global city_editor
+    global province_editor
+    global zipcode_editor
+
+    # Create Text Boxes
+    f_name_editor = Entry(editor, width=30)
+    f_name_editor.grid(row=0, column=1, padx=20, pady=(10, 0))
+    l_name_editor = Entry(editor, width=30)
+    l_name_editor.grid(row=1, column=1, padx=20)
+    address_editor = Entry(editor, width=30)
+    address_editor.grid(row=2, column=1, padx=20)
+    city_editor = Entry(editor, width=30)
+    city_editor.grid(row=3, column=1, padx=20)
+    province_editor = Entry(editor, width=30)
+    province_editor.grid(row=4, column=1, padx=20)
+    zipcode_editor = Entry(editor, width=30)
+    zipcode_editor.grid(row=5, column=1, padx=20)
+    
+    # Create Text Box Labels
+    f_name_label = Label(editor, text="First Name")
+    f_name_label.grid(row=0, column=0, pady=(10, 0))
+    l_name_label = Label(editor, text="Last Name")
+    l_name_label.grid(row=1, column=0)
+    address_label = Label(editor, text="Address")
+    address_label.grid(row=2, column=0)
+    city_label = Label(editor, text="City")
+    city_label.grid(row=3, column=0)
+    province_label = Label(editor, text="Province")
+    province_label.grid(row=4, column=0)
+    zipcode_label = Label(editor, text="Zipcode")
+    zipcode_label.grid(row=5, column=0)
+
+    # Loop through results
+    for record in records:
+        f_name_editor.insert(0, record[0])
+        l_name_editor.insert(0, record[1])
+        address_editor.insert(0, record[2])
+        city_editor.insert(0, record[3])
+        province_editor.insert(0, record[4])
+        zipcode_editor.insert(0, record[5])
+
+    # Create Save Button
+    save_btn = Button(editor, text="Save Record", command=update)
+    save_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+    
+   
 
 # Create Function to Delete A Record
 def delete():
@@ -80,10 +187,9 @@ def query():
     # Create a cursor
     c = conn.cursor()
 
-    # Query thte database
+    # Query the database
     c.execute("SELECT *, oid FROM addresses")
     records = c.fetchall()
-    print(records)
 
     # Loop through results
     print_records = ''
@@ -91,7 +197,7 @@ def query():
         print_records += str(record[0]) + " " + str(record[1]) + " " + "\t" + str(record[6]) + '\n'
 
     query_label = Label(root, text=print_records)
-    query_label.grid(row=11, column=0, columnspan=2)
+    query_label.grid(row=12, column=0, columnspan=2)
 
     # Commit Change
     conn.commit()
@@ -128,8 +234,7 @@ province_label = Label(root, text="Province")
 province_label.grid(row=4, column=0)
 zipcode_label = Label(root, text="Zipcode")
 zipcode_label.grid(row=5, column=0)
-
-delete_box_label = Label(root, text="ID Number")
+delete_box_label = Label(root, text="Select ID")
 delete_box_label.grid(row=9, column=0)
 
 # Create Submit Button
@@ -143,6 +248,10 @@ query_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
 # Create a Delete Button
 delete_btn = Button(root, text="Delete Record", command=delete)
 delete_btn.grid(row=10, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
+
+# Create a Update Button
+edit_btn = Button(root, text="Edit Record", command=edit)
+edit_btn.grid(row=11, column=0, columnspan=2, padx=10, pady=10, ipadx=145)
 
 # Commit Changes
 conn.commit()
---
+++ weather.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import sys
+import requests
+import json
+
+sys.path.insert(0, "/usr/lib/python3/dist-packages")
+
+root = Tk()
+root.title("Learn to Code!")
+root.geometry('400x50')
+
+# https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=71543A65-B438-41BB-A06E-8BA185D0CECD
+
+
+
+
+
+root.mainloop()
